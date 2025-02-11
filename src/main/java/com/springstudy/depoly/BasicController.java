919ceb1f77656d6319bf76fd37b86151b1c44222
ghkdwnsdlr <imbanana15@gmail.com>
2025-02-11 22:06:00 +0900

    s

--- src/main/java/com/springstudy/depoly/BasicController.java
+++ src/main/java/com/springstudy/depoly/BasicController.java
@@ -1,4 +1,14 @@
 package com.springstudy.depoly;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BasicController {
+    @GetMapping("/h")
+    @ResponseBody
+    public String h() {
+        return "hello";
+    }
 }
